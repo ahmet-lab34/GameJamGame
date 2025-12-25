280c3b72ba518a31eef91b5fc5fc34b209d20056
ahmet-lab34 <ahmetcuma321@gmail.com>
2025-12-25 23:34:50 +0300

    adsd

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,117 @@
+using System.Numerics;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System;
+
+public class PlayerScript : MonoBehaviour
+{
+    private GroundCHK GroundCheck;
+    private Rigidbody2D rb;
+
+    public Animator animator;
+    private PlayerInput input;
+    private InputAction jumpAction;
+    [HideInInspector] public float Horizontal;
+
+    public bool IsFacingRight;
+
+    [SerializeField] private PlayerUIScript PlayerUIScript;
+
+    public PlayerStats movingStats = new PlayerStats();
+    private PlayerStats originalStats = new PlayerStats();
+
+    public Numerics playerNumbers = new Numerics();
+
+    [Serializable]
+    public class PlayerStats
+    {
+        public float activeMoveSpeed = 5f;
+        public float jumpHeight = 10f;
+    }
+
+    [Serializable]
+    public class Numerics
+    {
+        public int playerHealth = 3;
+    }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        input = GetComponent<PlayerInput>();
+
+        animator = GetComponentInChildren<Animator>();
+
+        GroundCheck = FindFirstObjectByType<GroundCHK>();
+        PlayerUIScript = FindFirstObjectByType<PlayerUIScript>();
+
+        jumpAction = input.actions.FindAction("Jump");
+
+        Time.timeScale = 1f;
+    }
+
+    void Start()
+    {
+        originalStats.activeMoveSpeed = movingStats.activeMoveSpeed;
+        originalStats.jumpHeight = movingStats.jumpHeight;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new UnityEngine.Vector2(Horizontal * movingStats.activeMoveSpeed, rb.linearVelocity.y);
+    }
+
+    void Update()
+    {
+        Horizontal = Input.GetAxisRaw("Horizontal");
+
+        animator.SetBool("IsRunning", Horizontal > 0 || Horizontal < 0);
+        animator.SetFloat("yVelocity", rb.linearVelocity.y);
+
+        if (GroundCheck.Grounded)
+        {
+            animator.SetBool("IsJumping", false);
+            Debug.Log("Character is Grounded");
+        }
+
+        if (jumpAction.triggered && GroundCheck.Grounded)
+        {
+            rb.AddForce(UnityEngine.Vector2.up * movingStats.jumpHeight, ForceMode2D.Impulse);
+        }
+
+        if (!GroundCheck.Grounded)
+        {
+            animator.SetBool("IsJumping", true);
+        }
+
+        if ((IsFacingRight && Horizontal < 0f) || (!IsFacingRight && Horizontal > 0f))
+        {
+            Flip();
+        }
+
+    }
+
+    public void GetHit()
+    {
+        playerNumbers.playerHealth -= 1;
+
+        if (playerNumbers.playerHealth <= 0)
+        {
+            PlayerUIScript.Die();
+        }
+        Debug.Log("Player Got Hit !");
+    }
+
+    private void Flip()
+    {
+        IsFacingRight = !IsFacingRight;
+        UnityEngine.Vector2 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+    }
+}
---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.Video;
+
+public class UIScript : MonoBehaviour
+{
+    [SerializeField] private GameObject PlayerObject;
+    [SerializeField] private GameObject DeathPanel;
+    [SerializeField] private GameObject MainMenu;
+    [SerializeField] private GameObject GameUI;
+    [SerializeField] private GameObject Settings;
+    [SerializeField] private GameObject GameMap1;
+    [SerializeField] private Button RestartButton;
+    [SerializeField] private Button MainMenuButton;
+    [SerializeField] private Button MainPlayButton;
+    [SerializeField] private Button MainOptionsButton;
+    [SerializeField] private Button MainExitButton;
+    [SerializeField] private Button OptionsBackButton;
+
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("SkipMainMenu", 0) == 1)
+        {
+            MainMenu.SetActive(false);
+            GameMap1.SetActive(true);
+            GameUI.SetActive(true);
+            Time.timeScale = 1f;
+            PlayerPrefs.SetInt("SkipMainMenu", 0);
+        }
+        else
+        {
+            MainMenu.SetActive(true);
+        }
+        RestartButton.onClick.AddListener(() => OnButtonPressed(RestartButton));
+        MainMenuButton.onClick.AddListener(() => OnButtonPressed(MainMenuButton));
+
+        MainPlayButton.onClick.AddListener(() => OnButtonPressed(MainPlayButton));
+        MainOptionsButton.onClick.AddListener(() => OnButtonPressed(MainOptionsButton));
+        OptionsBackButton.onClick.AddListener(() => OnButtonPressed(OptionsBackButton));
+        MainExitButton.onClick.AddListener(() => OnButtonPressed(MainExitButton));
+    }
+    public void RestartScene()
+    {
+        PlayerPrefs.SetInt("SkipMainMenu", 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void UIDie()
+    {
+        DeathPanel.SetActive(true);
+        PlayerObject.SetActive(false);
+        Time.timeScale = 0f;
+    }
+    void OnButtonPressed(Button button)
+    {
+        if (button == RestartButton)
+        {
+            Debug.Log("Just the Restart Button Is Pressed !");
+            RestartScene();
+        }
+        if (button == MainMenuButton)
+        {
+            Scene currentScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(currentScene.name);
+        }
+        if (button == MainPlayButton)
+        {
+            StartGameFromMenu();
+        }
+        if (button == MainOptionsButton)
+        {
+            Settings.SetActive(true);
+            MainMenu.SetActive(false);
+        }
+        if (button == OptionsBackButton)
+        {
+            MainMenu.SetActive(true);
+            Settings.SetActive(false);
+        }
+        if (button == MainExitButton)
+        {
+            Application.Quit();
+        }
+    }
+    private void StartGameFromMenu()
+    {
+        GameMap1.SetActive(true);
+        GameUI.SetActive(true);
+        MainMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
